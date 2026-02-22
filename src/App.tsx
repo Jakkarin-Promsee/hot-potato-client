53c0e7abfe2b3f7b595474788ec6da27a5f3ef94
Night <jakkrinmes@gmail.com>
2026-02-23 02:59:19 +0700

    feat: install typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useMemo } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600 underline">
+      Tailwind is working! s ss sss
+    </h1>
+  );
+}
+
+export default App;
