097d29be85d3f240860fdd930afc5714a9c7b528
Night <jakkrinmes@gmail.com>
2026-02-23 18:33:48 +0700

    feat: complete basis css and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React, { useState, useMemo } from "react";
-import "./App.css";
+// import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <h1 className="text-3xl font-bold text-blue-600 underline">
-      Tailwind is working! s ss sss
-    </h1>
-  );
-}
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    {/* <TooltipProvider>
+      <Toaster />
+      <Sonner /> */}
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+
+    {/*</TooltipProvider> */}
+  </QueryClientProvider>
+);
 
 export default App;
